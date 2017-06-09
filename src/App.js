2871613e5cdde6141e0cc716ceba40435fd6db7f
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-09 16:58:00 +1000

    Navigation Drawer Function Added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import React from 'react';
-import logo from './logo.jpg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.jpg'
+import './App.css'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import RaisedButton from 'material-ui/RaisedButton'
+import MenuItem from 'material-ui/MenuItem';
 
 injectTapEventPlugin()
-const style = {
-  margin: 12,
-};
-      const App = () => (
-        <div>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return(
+      <div>
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <AppBar
           title="Trove Newspapers"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onTouchTap={this.handleToggle}
         />
-
       </MuiThemeProvider>
-      <MuiThemeProvider>
-      <RaisedButton label="Test Button"
-                    onMouseUp={()=>{alert('Mouse Raised Button Works!')}}
-                    primary={true}
-      />
-      </MuiThemeProvider>
-    </div>
-    );
+        <MuiThemeProvider>
+          <div>
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.open}
+              onRequestChange={(open) => this.setState({open})}
+            >
+              <MenuItem onTouchTap={this.handleClose}>New South Wales</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Victoria</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>South Australia</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Queensland</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Western Australia</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Tasmania</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>A.C.T.</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Northern Territory</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>National/International</MenuItem>
+            </Drawer>
+          </div>
+        </MuiThemeProvider>
+      </div>
 
+    )
+  }
+}
 
-export default App;
+// export default App;
---
+++ src/config/endpoints.js
@@ -0,0 +1,1 @@
+export const relayApi = 'https://api.graph.cool/relay/v1/cj3pemcfm67hh0149308myucb'
