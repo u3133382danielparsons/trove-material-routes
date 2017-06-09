bfd718c09b31edebfbb1e7d6ec163b5e13495ea0
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-09 14:59:07 +1000

    Raised Button added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 
 injectTapEventPlugin()
+const style = {
+  margin: 12,
+};
       const App = () => (
+        <div>
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <AppBar
           title="Trove Newspapers"
-          iconClassNameRight="muidocs-icon-navigation-expand-more" 
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
         />
+
       </MuiThemeProvider>
+      <MuiThemeProvider>
+      <RaisedButton label="Test Button"
+                    onMouseUp={()=>{alert('Raised Button Works!')}}
+                    onTouchEnd={()=>{alert('Raised Button Works!')}}
+                    primary={true}
+      />
+      </MuiThemeProvider>
+    </div>
     );
 
 
